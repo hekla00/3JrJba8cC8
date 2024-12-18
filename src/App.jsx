ace681b003e127abc7a4ba3bff3cb31b84d8ad98
Hekla <heklasamuelsdottir00@gmail.com>
2024-12-18 17:10:49 +0000

    Header added and configuration for tailwind fixed close #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Header from './components/Header';
+import React from 'react';
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='App'>
+      <Header />
     </div>
   );
 }
