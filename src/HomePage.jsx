8e2f6a424875ea0200ffd903ca2f9051012ade69
Hekla <heklasamuelsdottir00@gmail.com>
2024-12-21 18:17:21 +0000

    Segment component added close #11

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,19 +3,24 @@ import HeroSection from './components/HeroSection/HeroSection';
 import Footer from './components/Footer/Footer';
 import List from './components/List/List';
 import { fetchPosts } from './api/fetchPosts';
+import Segment from './components/Segment';
 
 function HomePage() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [activeCategory, setActiveCategory] = useState('top');
 
-  const loadStories = async () => {
+  // Function to load the stories
+  const loadStories = async (category) => {
     setLoading(true);
     setError(null);
 
     try {
-      const stories = await fetchPosts();
+      // Fetch the posts based on the category
+      const stories = await fetchPosts(category);
       setPosts(stories);
+      //   Set loading to false when the posts are fetched
       setLoading(false);
     } catch (error) {
       setError(error.message);
@@ -23,15 +28,25 @@ function HomePage() {
     }
   };
 
+  // Load the stories when the page loads
   useEffect(() => {
-    loadStories();
-  }, []);
+    loadStories(activeCategory);
+  }, [activeCategory]);
+
+  //   function to handle category change
+  const handleCategoryChange = (category) => {
+    setActiveCategory(category);
+  };
 
   return (
     <div className='bg-white py-24 sm:py-32'>
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
         <HeroSection />
         <div className='mx-auto mt-2 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-5 sm:mt-10 sm:pt-10 lg:mx-0 lg:max-w-none lg:grid-cols-3'></div>
+        <Segment
+          activeCategory={activeCategory}
+          onCategoryChange={handleCategoryChange}
+        />
         {/* Loading / Error State */}
         {loading && <p>Loading posts...</p>}
         {error && <p>{error}</p>}
