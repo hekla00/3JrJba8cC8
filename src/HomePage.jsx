65f3f9632eb3e34ebc7cc70bf9d999349f8e8dd5
Hekla <heklasamuelsdottir00@gmail.com>
2024-12-21 17:53:30 +0000

    List component added & new & top posts fetched from api close #9

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,31 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import HeroSection from './components/HeroSection';
 import Footer from './components/Footer';
+import List from './components/List';
+import { fetchPosts } from './api/fetchPosts';
 
-export default function HomePage() {
-  const [data, setData] = useState(null);
-  // Fetching data from the Hacker News API to test connection
+function HomePage() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const loadStories = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const stories = await fetchPosts();
+      setPosts(stories);
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    fetch('https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty')
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error('Error fetching data:', error));
+    loadStories();
   }, []);
 
   return (
     <div className='bg-white py-24 sm:py-32'>
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
         <HeroSection />
-        <div className='mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
-          {data && (
-            <div>
-              <h3 className='text-lg font-semibold'>{data.title}</h3>
-              <p>{data.text}</p>
-            </div>
-          )}
-        </div>
+        <div className='mx-auto mt-2 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-5 sm:mt-10 sm:pt-10 lg:mx-0 lg:max-w-none lg:grid-cols-3'></div>
+        {/* Loading / Error State */}
+        {loading && <p>Loading posts...</p>}
+        {error && <p>{error}</p>}
+        {/* Display the posts */}
+        <List posts={posts} />
       </div>
       <Footer />
     </div>
   );
 }
+
+export default HomePage;
